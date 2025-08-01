9d93986b6d169e4c14d4eca3f79cbc47ed648d3b
Steopa2001 <moraru495@gmail.com>
2025-08-01 16:50:23 +0200

    feat: create routers/posts.js

--- routers/posts.js
+++ routers/posts.js
@@ -1,3 +1,36 @@
 // Importo exxpress
 const express = require('express');
 const router = express.router();
+
+// GET /posts → mostra tutti i post
+router.get("/", (req, res) => {
+  res.send("Lista dei post");
+});
+
+// GET /posts/:id → mostra un singolo post
+router.get("/:id", (req, res) => {
+  res.send(`Visualizzazione del post ${req.params.id}`);
+});
+
+// POST /posts → crea un nuovo post
+router.post("/", (req, res) => {
+  res.send("Creazione di un nuovo post");
+});
+
+// PUT /posts/:id → modifica completa del post
+router.put("/:id", (req, res) => {
+  res.send(`Modifica completa del post ${req.params.id}`);
+});
+
+// PATCH /posts/:id → modifica parziale del post
+router.patch("/:id", (req, res) => {
+  res.send(`Modifica parziale del post ${req.params.id}`);
+});
+
+// DELETE /posts/:id → elimina un post
+router.delete("/:id", (req, res) => {
+  res.send(`Cancellazione del post ${req.params.id}`);
+});
+
+//esporto router 
+module.exports = router;
